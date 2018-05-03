0439770c0687ffa96c45adee38c9409229d72bbb
Dmitrii <javafvr@yahoo.com>
2018-05-03 22:56:42 +0500

    work with portfolio

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
 	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
 		highlightSelector: 'nav a'
 	});
+
+
+	// mixItUp2 фильтр работ в портфолио
+	$('#portfolio-projects').mixItUp();
+
+
+	// FancyBox
+	$(".fancybox").fancybox({
+			helpers: {
+				overlay: {
+					locked: false
+				}
+			}
+	});
 });
